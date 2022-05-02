5670078447923ab10e799269e0e8ba6e0d1f081f
JF-PS <jfps.dev21@gmail.com>
2022-05-02 14:38:17 +0200

    first commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,8 @@
+import { Button as MuiButton } from "@mui/material";
+import { styled } from "@mui/system";
+
+const Button = styled(MuiButton)({
+  border: "3px solid blue",
+});
+
+export default Button;
